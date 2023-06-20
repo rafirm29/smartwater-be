8c541753db9d2f700612c8292f2e060787e6a424
rafirm29 <13519154@std.stei.itb.ac.id>
2023-06-20 14:46:36 +0700

    feat: add anomalies and status to pool model

--- models/pool.py
+++ models/pool.py
@@ -1,12 +1,21 @@
+from enum import Enum
 from pydantic import BaseModel, Field
 from bson import ObjectId
 from typing import List
+
+
+class StatusEnum(str, Enum):
+    NORMAL = "normal"
+    WARNING = "warning"
+    DANGER = "danger"
 
 
 class Pool(BaseModel):
     id: str = Field(default_factory=ObjectId, alias='_id')
     user_email: str
     name: str
+    status: StatusEnum = Field(default=StatusEnum.NORMAL)
+    anomaly: int = 0
     sensor: List[str] = []
 
     class Config:
--- schemas/pool.py
+++ schemas/pool.py
@@ -1,10 +1,14 @@
 from pydantic import BaseModel
 from typing import List
 
+from models.pool import StatusEnum
+
 
 class PoolData(BaseModel):
     id: str
     name: str
+    status: StatusEnum
+    anomaly: int
     sensor: List[str]
 
 
