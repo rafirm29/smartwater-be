58f267c1974966efe50476388469cebacbb97df6
rafirm29 <13519154@std.stei.itb.ac.id>
2023-07-08 15:27:22 +0700

    feat: connect to dpeloyed db

--- config/config.py
+++ config/config.py
@@ -5,6 +5,8 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     PATH_TO_PRIVATE_KEY: str
+    MONGODB_ADMIN: str
+    MONGODB_PASSWORD: str
 
     class Config:
         env_file = ".env"
--- db/database.py
+++ db/database.py
@@ -4,7 +4,10 @@ from models.user import User
 from models.pool import Pool
 from models.history import History
 
-client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://localhost:27017')
+from config.config import settings
+
+client = motor.motor_asyncio.AsyncIOMotorClient(
+    f'mongodb+srv://{settings.MONGODB_ADMIN}:{settings.MONGODB_PASSWORD}@smartwater-cluster.s5r9b9h.mongodb.net')
 db = client['smartwater-db']
 collection_user: Collection[User] = db['user']
 collection_pool: Collection[Pool] = db['pool']
