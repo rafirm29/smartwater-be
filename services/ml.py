28e5a15f8a0e8079357956853343c105040746d3
rafirm29 <13519154@std.stei.itb.ac.id>
2023-07-08 20:03:54 +0700

    feat: add docker

--- services/ml.py
+++ services/ml.py
@@ -1,8 +1,8 @@
-import tflite_runtime.interpreter as tf
+import tensorflow as tf
 
 # Path to the .tflite file
 tflite_model_path = "ModelGRUBest.tflite"
 
 # Load the TFLite model
-interpreter = tf.Interpreter(model_path=tflite_model_path)
+interpreter = tf.lite.Interpreter(model_path=tflite_model_path)
 interpreter.allocate_tensors()
