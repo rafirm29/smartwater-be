b3156068c2bc218c4f9e269f851dcf545406a0d1
rafirm29 <13519154@std.stei.itb.ac.id>
2023-07-08 18:32:37 +0700

    fix: update tensorflow package to reduce size

--- services/ml.py
+++ services/ml.py
@@ -1,8 +1,8 @@
-import tensorflow as tf
+import tflite_runtime.interpreter as tf
 
 # Path to the .tflite file
 tflite_model_path = "ModelGRUBest.tflite"
 
 # Load the TFLite model
-interpreter = tf.lite.Interpreter(model_path=tflite_model_path)
+interpreter = tf.Interpreter(model_path=tflite_model_path)
 interpreter.allocate_tensors()
